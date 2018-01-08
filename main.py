7adb0cfe8edb57e7d3c58195abeb492227c875b1
MihailoBGD <mnsbgd@yahoo.com>
2018-01-08 12:58:00 -0500

    testedoutneedtoaddpersistenceandimdone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import jinja2 
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -20,37 +19,6 @@ def index():
         verify_error = ""
         email_error = "" 
 
-        # error messages
-        required_error = "This field is required"
-        password_re_enter = "Please re-enter password"
-        charcount_error = "Entry must be between 3 and 20 characters"
-        passmatch_error = "Password and password confirmation must match"
-        invalid_email_error = "Email must contain no spaces, one @ symbol and one . symbol"
-
-
-        # username validation
-'''
-        if not username:
-            #username_error = required_error
-        if len(username) > 0 and len(username) < 3 or len(username) > 20:
-            #username_error = charcount_error
-
-        # password validation
-
-        if not password:
-            #password_error = required_error
-        if len(password) > 0 and len(password) < 3 or len(password) > 20:
-            #password_error = charcount_error
-
-        # password re-entry verification
-
-        if not verify:
-            verify_error = required_error
-        if len(verify) > 0 and len(verify) < 3 or len(verify) > 20:
-            verify_error = charcount_error    
-'''
-
-        if 
         # username validation
         username_error = reqfield_charcount_errorcheck(username)
 
@@ -60,21 +28,22 @@ def index():
         # password re-entry verification   
         verify_error = reqfield_charcount_errorcheck(verify)
 
+        if email:
+            email_error = valid_email(email)
+
         # match password to verify
 
         if password != verify:
-            password_error = passmatch_error 
-            verify_error = passmatch_error
-        
+            password_error = "Passwords don't match"
+            verify_error = password_error
+
         if not (username_error or password_error or verify_error or email_error):
             return render_template('welcome.html',username=username)
-        
+
         return render_template('signup_form.html', username_error = username_error, 
         errorpass = password_error, verr_orify = verify_error, err_mail = email_error)
 
     return render_template('signup_form.html')
-
-
 
 def reqfield_charcount_errorcheck(field):
     error = ""
@@ -84,31 +53,10 @@ def reqfield_charcount_errorcheck(field):
         error = "Entry must be between 3 and 20 characters"
     return error 
 
-  def valid_email(user_input):
-    if user_input.count('@') > 1:
-       
-        error = "Email must contain no spaces, one @ symbol and one . symbol"
-    if     
-    return error
-
-
-'''
 def valid_email(user_input):
-    for look_for in ['@', '.']:
-        count = username_error.count(look_for)
-        if count > 1:
-            # error = "Email must contain no spaces, one @ symbol and one . symbol"
+    error = ""
+    if user_input.count('@') > 1 or user_input.count('.') > 1 or user_input.count(' ') > 0:
+        error = "Email must contain no spaces, one @ symbol and one . symbol"
     return error
-'''
-
-'''
-def count(actual_string, sub, start = 0, end = len(string)):
-    count = 0
-    for character in range(start, end, 1):
-        if actual_string[character] == sub:
-            count += 1
-
-    return count
-'''
 
 app.run()
